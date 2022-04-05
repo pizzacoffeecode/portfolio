7873a6a8c7dd6fb2c269aeba42fb5f8444626dee
Fireal1983 <42722796+Fireal1983@users.noreply.github.com>
2022-04-05 13:48:41 -0400

    test

--- frontend/src/components/LevelUp/leveldata.js
+++ frontend/src/components/LevelUp/leveldata.js
@@ -20,6 +20,16 @@ export const leveldata = [
         },
     },
     {
+        skill: "CRUD",
+        level: 5,
+        color1: "rgba(0, 194, 255, 1)",
+        color2: "rgba(250, 0, 255, 1)",
+        subskill: {
+            skill: "",
+            level: 0
+        },
+    },
+    {
         skill: "React JS",
         level: 9,
         color1: "rgba(0, 194, 255, 1)",
@@ -32,6 +42,16 @@ export const leveldata = [
     {
         skill: "React Native",
         level: 6,
+        color1: "rgba(0, 194, 255, 1)",
+        color2: "rgba(250, 0, 255, 1)",
+        subskill: {
+            skill: "",
+            level: 0
+        },
+    },
+    {
+        skill: "Redux",
+        level: 4,
         color1: "rgba(0, 194, 255, 1)",
         color2: "rgba(250, 0, 255, 1)",
         subskill: {
