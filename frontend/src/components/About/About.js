c716bb6bf572d61b6a6396e0f737d4148f332d62
Fireal <david.bushen@hotmail.co.uk>
2022-07-06 11:09:35 -0400

    07-06-2022 : 1109

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -33,19 +33,23 @@ const About = () => {
                 About Me
             </SectionTitle>
             <SectionTextFull>
-                I'm a <i>proudly</i> self-taught, web and application developer.
+                I'm a <i>proudly</i> self-taught, web and mobile application developer.
             </SectionTextFull>
             <SectionTextFull>
                 In 2020 I decided to take the leap and leave my former profession to
-                turn my love of coding into a full-time career. Although my former experience is not as a developer, I have a documented track-record of success, and bring
-                a skill-set that will benefit a any professional team environment.
+                turn my love of coding into a full-time career. Although my previous experience is not as a developer, I have a documented track-record of success, and bring
+                a skill-set that will benefit any professional team environment.
             </SectionTextFull>
             <SectionTextFull>
-                My primary languages are JavaScript and C#, in addition to various frameworks including React / React Native, NodeJS, and Unity. I'm at the stage now where I
-                understand the full architecture of an application and can implement the stack for the frontend, backend, api, and database of a project.
+                My primary languages are C#, JavaScript, and TypeScript, in addition to various frameworks such as React / React Native, NodeJS runtime, and Unity.
             </SectionTextFull>
             <SectionTextFull>
-                I'm currently freelancing, but would really like to collaborate in a professional environment as an entry level developer.
+                I mainly work with the MERN stack, but if your project requires something else, I love to learn new languages and technologies,
+                and can get up to speed quickly. I'm proficient at pixel-perfect frontend development, love to create my own components, and know how to write efficient code.
+                Additionally, I can also implement the backend requirements such as REST APIs or databases.
+            </SectionTextFull>
+            <SectionTextFull>
+                I'm currently freelancing, but would really like to collaborate in a professional environment as an entry level developer or intern.
                 If you think I might make a good fit for your next project or would like to learn more about me please feel free to <span type="button" id="contacts__link"> reach out </span>
                 or check out my <span type="button" id="skills__link">skills</span> and <span type="button" id="projects__link">projects</span> sections below...
             </SectionTextFull>
